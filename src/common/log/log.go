970d8d55793059e816534e56417d03bcae7635c2
葬爱 <zangleai>
2021-12-01 18:06:17 +0800

    balabala

--- src/common/log/log.go
+++ src/common/log/log.go
@@ -2,191 +2,145 @@ package log
 
 import (
 	"fmt"
-	"io"
+	"github.com/utahta/go-cronowriter"
+	"io/ioutil"
 	"os"
-	"path"
 	"runtime"
 	"strconv"
-	"sync"
+	"strings"
 	"time"
 )
 
-const (
-	colorRed    = 31
-	colorYellow = 33
-	colorBlue   = 34
-
-	levelT = "[Info] "
-	levelE = "[Error] "
-	levelW = "[Warn] "
-
-	defaultFileSize = 60 * 1024 * 1024
-	minFileSize     = 1 * 1024 * 1024
-	defaultLogDir   = "log"
-	defaultLogName  = "default.log"
-
-	logTypeStd logType = iota + 1
-	logTypeFile
-)
-
-type (
-	logType int
-	myLog   struct {
-		sync.Once
-		sync.Mutex
-		outs     map[logType]io.Writer //writer集合
-		file     *os.File              //文件句柄
-		fileName string                //日志名
-		dir      string                //日志存放路径
-		size     int64                 //单个日志文件的大小限制
-	}
-)
-
 var (
-	defaultLogger = &myLog{}
+	errorRed   = 31
+	warnYellow = 33
+	infoBlue   = 34
 )
 
-func (m *myLog) init() {
-	if m.dir == "" {
-		m.dir = defaultLogDir
-	}
-	if m.fileName == "" {
-		m.fileName = defaultLogName
-	}
-	if m.size == 0 {
-		m.size = defaultFileSize
-	} else {
-		if m.size < minFileSize {
-			panic(fmt.Sprintf("invalid size: %d", m.size))
-		}
-	}
-
-	if m.outs == nil {
-		m.outs = make(map[logType]io.Writer)
-	}
-	if !isExist(m.dir) {
-		if err := os.Mkdir(m.dir, 0777); err != nil {
-			panic(err)
-		}
-	}
-	name := path.Join(m.dir, m.fileName)
-	file, err := os.OpenFile(name, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
-	if err != nil {
-		panic(err)
-	}
-
-	m.file = file
-	m.outs[logTypeStd] = os.Stdout
-	m.outs[logTypeFile] = file
+type Log struct {
+	w           *cronowriter.CronoWriter
+	preFit      string
+	write2File  bool
+	write2Front bool
 }
 
-func (m *myLog) checkLogSize() {
-	if m.file == nil {
-		return
-	}
-	m.Lock()
-	defer m.Unlock()
-	fileInfo, err := m.file.Stat()
-	if err != nil {
-		panic(err)
-	}
-	if m.size > fileInfo.Size() {
-		return
-	}
-	//需要分割
-	newName := path.Join(m.dir, time.Now().Format("2006_01_02_15:04:03")+".log")
-	name := path.Join(m.dir, m.fileName)
-
-	err = os.Rename(name, newName)
-	if err != nil {
-		panic(err)
-	}
-
-	file, err := os.OpenFile(name, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
-	if err != nil {
-		panic(err)
-	}
-
-	m.file.Close()
-	m.file = file
-	m.outs[logTypeFile] = file
-	return
-}
-
-func (m *myLog) write(level string, content string) {
-	m.checkLogSize()
-	var colorText int
-	switch level {
-	case levelT:
-		colorText = colorBlue
-	case levelW:
-		colorText = colorYellow
-	case levelE:
-		colorText = colorRed
-	}
-
-	for k, wr := range m.outs {
-		if k == logTypeStd {
-			fmt.Fprintf(wr, setColor(content, colorText))
-		} else {
-			fmt.Fprintf(wr, content)
-		}
-	}
-}
-
-func InitLogger() {
-	defaultLogger.init()
-}
-
-//Info
-func Info(format string, v ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	timeStr := time.Now().Format("2006-01-02 15:04:05.0000") + " "
-	codeLine := "[" + timeStr + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelT + codeLine + fmt.Sprintf(format, v...) + "\n"
-	defaultLogger.write(levelT, content)
-}
-
-//Error
-func Error(format string, v ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	timeStr := time.Now().Format("2006-01-02 15:04:05.0000") + " "
-	codeLine := "[" + timeStr + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelE + codeLine + fmt.Sprintf(format, v...) + "\n"
-	defaultLogger.write(levelE, content)
-}
-
-//Warn
-func Warn(format string, v ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
-	timeStr := time.Now().Format("2006-01-02 15:04:05.0000") + " "
-	codeLine := "[" + timeStr + shortFileName(file) + ":" + strconv.Itoa(line) + "]"
-	content := levelW + codeLine + fmt.Sprintf(format, v...) + "\n"
-	defaultLogger.write(levelW, content)
-}
-
-func isExist(path string) bool {
-	_, err := os.Stat(path)
-	if err != nil {
-		if os.IsExist(err) {
-			return true
-		}
-		return false
-	}
-	return true
-}
-
-func shortFileName(file string) string {
-	short := file
-	for i := len(file) - 1; i > 0; i-- {
-		if file[i] == '/' {
-			short = file[i+1:]
-			break
-		}
-	}
-	return short
-}
+var defaultLog *Log
 
 func setColor(msg string, text int) string {
 	return fmt.Sprintf("%c[%dm%s%c[0m", 0x1B, text, msg, 0x1B)
+}
+
+func (this *Log) printLog(lev int, data string) {
+	_, file, line, ok := runtime.Caller(2)
+
+	var log = "["
+	if this.preFit != "" {
+		log = this.preFit + " "
+	}
+	log += time.Now().Format("2006-01-02 15:04:05") + " "
+	if ok {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		log += short
+		log += ":"
+		log += strconv.Itoa(line)
+	}
+	log += "]\t"
+	switch lev {
+	case errorRed:
+		log += "Error"
+		break
+	case warnYellow:
+		log += "Warn"
+		break
+	case infoBlue:
+		log += "Info"
+		break
+	}
+	log += ": "
+	log += data
+	log += "\n"
+
+	log = setColor(log, lev)
+	if this.write2File {
+		this.w.Write([]byte(log))
+	}
+	if this.write2Front {
+		print(log)
+	}
+}
+
+func Info(format string, v ...interface{}) {
+	defaultLog.printLog(infoBlue, fmt.Sprintf(format, v...))
+}
+
+func Error(format string, v ...interface{}) {
+	defaultLog.printLog(errorRed, fmt.Sprintf(format, v...))
+}
+
+func Warn(format string, v ...interface{}) {
+	defaultLog.printLog(warnYellow, fmt.Sprintf(format, v...))
+}
+
+func ListDir(dirPth string) (files []string, err error) {
+	files = make([]string, 0, 10)
+	dir, err := ioutil.ReadDir(dirPth)
+	if err != nil {
+		return nil, err
+	}
+	for _, fi := range dir {
+		if fi.IsDir() { // 忽略目录
+			continue
+		}
+		files = append(files, fi.Name())
+	}
+	return files, nil
+}
+
+func getTimeArr(start, end string) int64 {
+	timeLayout := "20060102"
+	loc, _ := time.LoadLocation("Local")
+	startUnix, _ := time.ParseInLocation(timeLayout, start, loc)
+	endUnix, _ := time.ParseInLocation(timeLayout, end, loc)
+	startTime := startUnix.Unix()
+	endTime := endUnix.Unix()
+	date := (endTime - startTime) / 86400
+	return date
+}
+
+func cleanLog(t1 interface{}) {
+	for {
+		select {
+		case <-t1.(*time.Ticker).C:
+			path, _ := os.Getwd()
+			today := time.Now().Format("20060102")
+			logs, _ := ListDir(path + "/log")
+			for _, logFile := range logs {
+				oldFile := strings.Replace(logFile, "log_", "", -1)
+				oldFile = strings.Replace(oldFile, ".txt", "", -1)
+				arr := getTimeArr(oldFile, today)
+				if arr >= 30 {
+					Info("delete old log file %s", path+"/log/"+logFile)
+					os.Remove(path + "/log/" + logFile)
+				}
+			}
+		}
+	}
+}
+
+func InitDefaultLog(logName string, write2Front bool, write2File bool) {
+	path, _ := os.Getwd()
+	defaultLog = &Log{}
+	defaultLog.preFit = ""
+	defaultLog.write2Front = write2Front
+	defaultLog.write2File = write2File
+	defaultLog.w = cronowriter.MustNew(path + "/log/log_" + logName + "_%Y%m%d.txt")
+
+	timeClean := time.NewTicker(time.Hour * 24)
+	go cleanLog(timeClean)
 }
--- src/main.go
+++ src/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "makemoney/common/log"
 
+func main() {
+	log.InitDefaultLog("test", true, true)
+	log.Info("sadasdadadasd")
+	log.Warn("sadasdadadasd")
+	log.Error("sadasdadadasd")
 }
