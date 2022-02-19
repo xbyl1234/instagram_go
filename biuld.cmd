bdd343e012f68a0ed3c4e529d00e378d9b53f93c
马化腾 <2821565468@qq.com>
2022-02-19 12:01:20 +0800

    short link

--- biuld.cmd
+++ biuld.cmd
@@ -5,6 +5,4 @@ set GOOS=linux
 @REM C:\Users\Administrator\go\go1.17.5\bin\go.exe build -i -o C:\Users\Administrator\Desktop\project\github\instagram_project\register makemoney/routine/register
 @REM C:\Users\Administrator\go\go1.17.5\bin\go.exe build -i -o C:\Users\Administrator\Desktop\project\github\instagram_project\refresh_cookies makemoney/routine/refresh_cookies
 @REM C:\Users\Administrator\go\go1.17.5\bin\go.exe build -i -o C:\Users\Administrator\Desktop\project\github\instagram_project\crawling_tags makemoney/routine/crawling_tags
-
-C:\Users\Administrator\go\go1.17.5\bin\go.exe build -o C:\Users\Administrator\Desktop\project\github\instagram_project\short_link C:\Users\Administrator\Desktop\project\github\instagram_project\src\routine\short_link\short_link.go
-
+C:\Users\Administrator\go\go1.17.5\bin\go.exe build -o C:\Users\Administrator\Desktop\project\github\instagram_project\bin\short_link makemoney/routine/short_link
